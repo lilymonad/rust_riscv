ebd8a759650cd53a807f4177072484f053629a68
Arthur Blanleuil <arthur.blanleuil@inria.fr>
2019-08-13 19:23:06 +0200

    Add lib documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! This crate is used for educational purposes only. Clone the
+//! [github repository](https://github.com/ablanleuil/rust_riscv) and create
+//! anything you want on top of this base. This crate was created only with the
+//! [RISC-V ISA specification](https://riscv.org/specifications/).
+
+/// The ISA module containing everything related to instruction format.
 pub mod isa;
+
+/// Contains implementations of simple RISC-V machines based on the standard.
 pub mod machine;
+
+/// Memory interface abstraction used to implement any memory interface you want.
 pub mod memory;
