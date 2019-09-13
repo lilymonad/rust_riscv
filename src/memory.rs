d753862834c2ba41e4759cc7ed1289c00c89bc51
Arthur Blanleuil <arthur.blanleuil@inria.fr>
2019-09-13 13:35:12 +0200

    Fix Vec<u8> Memory instance + add HashMap<> Memory instance

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 /// Represents the main memory.
 /// It can be implemented by any structure which can handle loads and stores.
 ///
@@ -82,7 +84,7 @@ impl Memory for Vec<u8> {
 /// Simple Memory implementation for u32 dynamic arrays (Vec<u32>)
 impl Memory for Vec<u32> {
     fn get_8(&self, addr:usize) -> u8 {
-        (self[addr / 4] >> ((addr % 4) * 8)) as u8
+        (self[addr / 4] >> ((3 - addr % 4) * 8)) as u8
     }
 
     fn get_16(&self, addr:usize) -> u16 {
@@ -126,5 +128,48 @@ impl Memory for Vec<u32> {
     fn set_32(&mut self, addr:usize, value:u32) {
         self.set_16(addr, ((value >> 16) & 0xFFFF) as u16);
         self.set_16(addr + 2, (value & 0xFFFF) as u16)
+    }
+}
+
+impl Memory for HashMap<usize, u32> {
+    fn get_8(&self, addr:usize) -> u8 {
+        let addr32 = addr / 4;
+        self.get(&addr32).map_or(0, | x | {
+            ((x >> (8 * (3 - addr % 4))) & 0xFF) as u8
+        })
+    }
+
+    fn get_16(&self, addr:usize) -> u16 {
+        ((self.get_8(addr) as u16) << 8) | (self.get_8(addr + 1) as u16)
+    }
+
+    fn get_32(&self, addr:usize) -> u32 {
+        let ret = ((self.get_16(addr) as u32) << 16) | (self.get_16(addr + 2) as u32);
+        ret
+    }
+
+    fn set_8(&mut self, addr:usize, value:u8) {
+        let addr32 = addr / 4;
+        match self.get_mut(&addr32) {
+            Some(x) => {
+                match addr % 4 {
+                    0 => *x = (*x & 0x00FFFFFF) | ((value as u32) << 24),
+                    1 => *x = (*x & 0xFF00FFFF) | ((value as u32) << 16),
+                    2 => *x = (*x & 0xFFFF00FF) | ((value as u32) << 8),
+                    _ => *x = (*x & 0xFFFFFF00) | ((value as u32) << 0),
+                }
+            },
+            None => { self.insert(addr32, (value as u32) << (8 * (3 - addr % 4))); }
+        }
+    }
+
+    fn set_16(&mut self, addr:usize, value:u16) {
+        self.set_8(addr, (value >> 8) as u8);
+        self.set_8(addr+1, value as u8)
+    }
+
+    fn set_32(&mut self, addr:usize, value:u32) {
+        self.set_16(addr, (value >> 16) as u16);
+        self.set_16(addr+2, value as u16)
     }
 }
